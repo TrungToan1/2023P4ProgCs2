9eb99e9b40612037afa78b7b1b93f9bd4fae2127
TrungToan1 <114745020+TrungToan1@users.noreply.github.com>
2023-05-08 10:06:40 +0200

    opdracht 2

---
+++ ConsoleMonGame/ConsoleMonGame/Skill.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMonGame
+{
+    internal class Skill
+    {
+        int damage;
+        int energyCost;
+        string name;
+    }
+}
