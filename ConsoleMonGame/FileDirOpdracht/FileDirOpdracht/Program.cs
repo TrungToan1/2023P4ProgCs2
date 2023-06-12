42eb268557df07ccd7ead8d5ff0395d80ec0d4dc
TrungToan1 <34772@ma-web.nl>
2023-06-12 10:54:34 +0200

    opdracht 9

---
+++ ConsoleMonGame/FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FileDirOpdracht
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo("C:\\Users\\Shado\\maap\\Bureaublad\\2023P4ProgCs2");
+
+            DirectoryInfo[] directories = dir.GetDirectories();
+            for (int i = 0; i < directories.Length; i++)
+            {
+                Console.WriteLine(directories[i].FullName);
+             
+            }
+
+
+            FileInfo[] files = dir.GetFiles();
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i].FullName);
+            }
+
+        }
+
+    }
+}
