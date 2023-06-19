09a132e10c4ba76a3177985b0db6615dbc640b83
TrungToan1 <34772@ma-web.nl>
2023-06-19 10:16:36 +0200

    FileDirOpracht

---
+++ FileDirOpdracht/FileDirOpdracht/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace FileDirOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "C:\\Users\\Shado\\maap\\Bureaublad\\2023P4ProgCs2";
+            DirectoryInfo dir = new DirectoryInfo(path);
+            if (dir.Exists )
+            {
+                Console.WriteLine("Directory exists");
+            }
+
+            FileInfo MyInfo = new FileInfo(path);
+
+            foreach ( FileInfo file in dir.GetFiles() )
+            {
+                Console.WriteLine(file.FullName);
+            }
+            string[] directories = Directory.GetDirectories("C:\\Users\\Shado\\maap\\Bureaublad\\2023P4ProgCs2");
+            foreach (string directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+        }
+    }
+}
