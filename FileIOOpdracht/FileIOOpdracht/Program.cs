4c48805763474ce0ac2fbe77b716215d42215ec6
TrungToan1 <34772@ma-web.nl>
2023-06-29 21:22:03 +0200

    FileIOOpdracht

---
+++ FileIOOpdracht/FileIOOpdracht/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace FileIOOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] content = File.ReadAllLines("leesdezefile.txt");
+            for (int i = 0; i < content.Length; i++)
+            {
+                Console.WriteLine(content[i]);
+            }
+            string outputDirectory = Path.Combine(Directory.GetCurrentDirectory(), "output");
+
+            Directory.CreateDirectory(outputDirectory);
+
+            string outputPath = Path.Combine(outputDirectory, "mijnnieuwefile.txt");
+
+            File.WriteAllText(outputPath, "dit is een nieuwe file.");
+
+            File.AppendAllText(outputPath, Environment.NewLine + "dit is een extra zin.");
+
+        }
+
+    }
+}
