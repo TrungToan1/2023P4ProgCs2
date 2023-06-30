528448944994c1fa2c547132381a2c4ce3f422f0
TrungToan1 <34772@ma-web.nl>
2023-06-30 10:21:39 +0200

    opdracht11.2

---
+++ SplitOpdracht/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace SplitOpdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = File.ReadAllText("stringsplit.txt");
+
+            string[] keyValue = content.Split(':');
+            Console.WriteLine(keyValue[0]);
+
+            string[] keyvalue2 = keyValue[1].Split(",");
+            foreach (string item in keyvalue2)
+            {
+                string trimmedItem = item.Trim();
+                if (!string.IsNullOrEmpty(trimmedItem))
+                    Console.WriteLine(trimmedItem);
+            }
+        }
+    }
+}
